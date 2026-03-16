ac631e102c4ab53d7c0150eba884e3457889f644
Tom Craggs <Thomas.Craggs@city.ac.uk>
2026-03-16 02:12:26 +0000

    feat: only show subsystems accessible to the account type

---
+++ src/main/java/ipos/sa/MainMenuController.java
@@ -0,0 +1,104 @@
+package ipos.sa;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+
+public class MainMenuController {
+
+    @FXML private Label loggedInUserLabel;
+    @FXML private Button accButton;
+    @FXML private Button catButton;
+    @FXML private Button ordButton;
+    @FXML private Button rptButton;
+
+    @FXML
+    public void initialize() {
+        try{
+            String name = UserSession.getInstance().getUsername();
+            loggedInUserLabel.setText(name);
+
+            applyPermissions(UserSession.getInstance().getType());
+
+
+
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void applyPermissions(String type){
+
+        if (type == null) return;
+        type = type.toLowerCase();
+
+        switch (type) {
+
+            case "director":
+            case "admin":
+                showButton(accButton);
+                showButton(catButton);
+                showButton(ordButton);
+                showButton(rptButton);
+                break;
+
+            case "merchant":
+                showButton(catButton);
+                showButton(ordButton);
+                break;
+
+            case "manager":
+                showButton(rptButton);
+                showButton(accButton);
+
+            default:
+                System.out.println("Unrecognised role, no subsystems activated");
+        }
+
+
+    }
+
+    private void showButton(Button btn) {
+        btn.setVisible(true);
+        btn.setManaged(true);
+    }
+
+    @FXML
+    private void handleLogout(ActionEvent event) {
+        try {
+            closeWindow();
+            System.out.println("Logging out...");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleAcc() {
+        System.out.println("Opening IPOS-ACC...");
+    }
+
+    @FXML
+    private void handleCat() {
+        System.out.println("Opening IPOS-CAT...");
+    }
+
+    @FXML
+    private void handleOrd() {
+        System.out.println("Opening IPOS-ORD...");
+    }
+
+    @FXML
+    private void handleRpt() {
+        System.out.println("Opening IPOS-RPT...");
+    }
+
+    private void closeWindow() {
+        Stage stage = (Stage) loggedInUserLabel.getScene().getWindow();
+        stage.close();
+    }
+}
